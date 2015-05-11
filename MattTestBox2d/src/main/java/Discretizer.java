49d8497d7253623935183ee1370a83d7de4004bc
Matt Sheen <mws262@cornell.edu>
2015-05-11 18:35:33 -0400

    starting to implement barycentric interpolation

---
+++ MattTestBox2d/src/main/java/Discretizer.java
@@ -0,0 +1,195 @@
+import java.util.ArrayList;
+
+/**
+ * This class deals with making a state discretizations, updating value functions, and interpolating when needed.
+ * 
+ * @author Matt
+ *
+ */
+
+
+public class Discretizer {
+	
+	/** Do I want to see diagnostic info? **/
+	public static boolean verbose = true;
+
+	private static ArrayList<float[]> StateLists = new ArrayList<float[]>();
+	
+	/** Keep a list of the bounds for each added dimension. All contained arrays should be 1x2 **/
+	private static ArrayList<float[]> stateBounds = new ArrayList<float[]>();
+	
+	/** Increments of each dimension discretization. Should always be same length ArrayList as stateBounds **/
+	private static ArrayList<Float> increments = new ArrayList<Float>();
+	
+	/** Number of gridpoints in each dimension **/
+	private static ArrayList<Integer> dimGridPts = new ArrayList<Integer>();
+	
+	private static int numDims = 0;
+	
+	private static int numStates = 0;
+
+	private static float[][] MeshedStates;
+	
+	public Discretizer() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public static void main(String args[]){
+		AddDimension(0f, 3f, 0.4f);
+		AddDimension(-1f, 2f, 0.6f);
+		AddDimension(6f, 7f, 0.5f);
+		AddDimension(4f,20f,5f);
+		AddDimension(4f,8.5f,8);
+		
+		MeshStateLists();
+	}
+	
+	/** Add a new dimension (e.g. calf angle, torso x position). Requires a minimum and maximum in a range and an increment. Works similar to min:increment:max in MATLAB. **/
+	public static void AddDimension(float min, float max, float increment){ //This one takes an increment and calculates number of required grid points
+		
+		//Make an individual list of the range. We'll worry about meshing the whole thing later.
+		float[] DiscreteList = new float[(int)Math.floor((max-min)/increment)+1];
+		
+		for (int i = 0; i<DiscreteList.length; i++){
+			DiscreteList[i] = min + i*increment;
+		}
+		StateLists.add(DiscreteList); //Add it to our running list of states.
+		if(verbose) System.out.println("Added a new dimension discretized into " + DiscreteList.length + " elements.");
+		
+		
+		//Make the bound list and increment list.
+		float[] bounds = {min, max};
+		StateLists.add(bounds);
+		increments.add(increment);
+		dimGridPts.add(DiscreteList.length);
+		//Record number of dimensions added.
+		numDims++;
+		//Record total number of states.
+		if(numStates == 0){
+			numStates = DiscreteList.length;
+		}else{
+			numStates *= DiscreteList.length;
+		}
+	}
+
+	/** Add a new dimension (e.g. calf angle, torso x position). Requires a minimum and maximum in a range and an increment. Works similar to linspace in MATLAB. **/
+	public static void AddDimension(float min, float max, int gridPts){ //This one takes the number of grid points and calculates the size of the required increment.
+		
+		//Make an individual list of the range. We'll worry about meshing the whole thing later.
+		float[] DiscreteList = new float[gridPts];
+		
+		float increment = (max-min)/(float)(gridPts-1);
+		for (int i = 0; i<DiscreteList.length; i++){
+			DiscreteList[i] = min + i*increment;
+		}
+		StateLists.add(DiscreteList); //Add it to our running list of states.
+		if(verbose) System.out.println("Added a new dimension discretized into " + DiscreteList.length + " elements.");
+		
+		
+		//Make the bound list and increment list.
+		float[] bounds = {min, max};
+		StateLists.add(bounds);
+		increments.add(increment);
+		dimGridPts.add(DiscreteList.length);
+		//Record number of dimensions added.
+		numDims++;
+		//Record total number of states.
+		if(numStates == 0){
+			numStates = DiscreteList.length;
+		}else{
+			numStates *= DiscreteList.length;
+		}
+	}
+	
+	/** Take all the previously added dimensions and create all possible combinations in an array. This may become unnecessary once barycentric interpolation is worked out. **/
+	public static void MeshStateLists(){
+
+		//Calculate the total number of states (e.g. a 3x3x3 space has 27 states).
+		int totStates = 1;
+		for (int i = 0; i<StateLists.size(); i++){
+			totStates *= StateLists.get(i).length;
+		}
+		if(verbose){
+			System.out.println("Number of dimensions (i.e. states): " + StateLists.size());
+			System.out.println("Total number of states in the discretization (i.e. all possible combinations): " + totStates);
+		}
+		
+		MeshedStates = new float[totStates][StateLists.size()]; //This is number of states x number of dimensions.
+		
+		int repeatedNumbers = 0;
+		int repeatedPatterns = 0;
+		int trackedRepetition = 1;
+		for (int i = StateLists.size()-1; i>=0; i--){ //Iterate through all the state lists contained in this arraylist. We'll count down just to keep continuity with other stuff I've done.
+			int counter = 0; //Track how far down the array we are on this pass.
+			
+			repeatedPatterns = totStates/(StateLists.get(i).length*trackedRepetition); //First time down, the pattern will be repeated
+			repeatedNumbers = trackedRepetition;
+			
+			trackedRepetition *= StateLists.get(i).length; //First time through, the pattern will be repeated by length/numstates. Next time it will only be repeated length/(numstates*numprevious states)
+			//e.g. first time through, might be: 1 2 3 1 2 3 1 2 3. Next time: 1 1 1 2 2 2 3 3 3.
+			
+			for (int j = 0; j< repeatedPatterns; j++){ //Now iterate through the number of repetitions of the pattern.
+				for(int k = 0; k<StateLists.get(i).length; k++){ //Now iterate through the individual values found in this particular state list.
+					for(int l = 0; l<repeatedNumbers;l++){ //Now iterate through the number of times each value is repeated.
+						MeshedStates[counter][i] = StateLists.get(i)[k];
+						
+						counter++;
+					}
+				}
+			}
+			//Make SURE we've populated the matrix up to the end. If not, I did the previous loops wrong.
+			if (counter != totStates){ throw new IndexOutOfBoundsException("When populating the state array, we ran out of values too early. Indexing issue.");}
+		}
+		
+		//Print fully meshed state list for debugging purposes.
+//		for (int i = 0; i< MeshedStates.length; i++){
+//			for (int j = 0; j<MeshedStates[0].length-1; j++){
+//				System.out.print(MeshedStates[i][j] + ",");
+//			}
+//			System.out.print(MeshedStates[i][MeshedStates[0].length-1]);
+//			System.out.println();
+//		}
+	}
+	
+	/** K nearest neighbor if barycentric doesn't work out **/
+	public static int FindNearestState(float[] state){	
+		return 1;
+	}
+	
+	/**Barycentric interpolation on the defined grid. Should return the nearest gridpoints on a simplex and weights for each **/
+	public static void BaryTerpolate(float[] state){
+		
+		/* Fix out of bounds states */ 
+		for (int i = 0; i<state.length; i++){ //State is lower than min bound.
+			if (state[i]<stateBounds.get(i)[0]){ 
+				//Report how off we are then project back.
+				if (verbose){ System.out.println("State " + i + " is " + (stateBounds.get(i)[0] - state[i]) / (stateBounds.get(i)[1] - stateBounds.get(i)[0]) + "% of full range below the min. Projected back.");}
+				state[i] = stateBounds.get(i)[0];
+				
+			}else if (state[i] > stateBounds.get(i)[1]){ //State is higher than max bound.
+				//Report how off we are then project back.
+				if (verbose){ System.out.println("State " + i + " is " + (state[i] - stateBounds.get(i)[1]) / (stateBounds.get(i)[1] - stateBounds.get(i)[0]) + "% of full range above the max. Projected back.");}
+				state[i] = stateBounds.get(i)[1];
+				
+			}	
+		}
+		
+		/* Translate and scale such that each grid cell is a unit cube. */
+		float[] scaledState = new float[state.length];
+		int[] xBase = new int[state.length];
+		float[] cubeX = new float[state.length];
+		
+		for (int i = 0; i< state.length; i++){
+			scaledState[i] = (state[i] - stateBounds.get(i)[0])/(stateBounds.get(i)[1] - stateBounds.get(i)[0])*dimGridPts.get(i); //Subtract out the low bound. Divide by full scale. Multiply by number of bins (bin-space representation).
+			xBase[i] = (int)Math.floor(scaledState[i]);
+			cubeX[i] = scaledState[i] - (float)xBase[i];
+		}
+		
+		
+		
+		
+		
+		
+	}
+	
+}
--- MattTestBox2d/src/main/java/QWOPInterface.java
+++ MattTestBox2d/src/main/java/QWOPInterface.java
@@ -34,7 +34,7 @@ public class QWOPInterface {
 		game = new QWOPGame();
 	}
 	
-	/* Make a new game */
+	/** Make a new game **/
 	public void NewGame(){
 		game.Setup(runRealtime);
 		m_world = game.getWorld();
@@ -44,27 +44,136 @@ public class QWOPInterface {
 		stepsInRun = 0;
 	}
 	
-	/* Dist/phys steps */
+	/** State getters below - The convention is to use x-y-theta **/
+	public float[] TorsoState(){
+		float[] state = new float[3];
+		state[0] = game.TorsoBody.getPosition().x;
+		state[1] = game.TorsoBody.getPosition().y;
+		state[2] = game.TorsoBody.getAngle();
+		
+		return state;
+	}
+	/** State getters below - The convention is to use x-y-theta **/
+	public float[] HeadState(){
+		float[] state = new float[3];
+		state[0] = game.HeadBody.getPosition().x;
+		state[1] = game.HeadBody.getPosition().y;
+		state[2] = game.HeadBody.getAngle();
+		
+		return state;
+	}
+	/** State getters below - The convention is to use x-y-theta **/
+	public float[] LThighState(){
+		float[] state = new float[3];
+		state[0] = game.LThighBody.getPosition().x;
+		state[1] = game.LThighBody.getPosition().y;
+		state[2] = game.LThighBody.getAngle();
+		
+		return state;
+	}
+	/** State getters below - The convention is to use x-y-theta **/
+	public float[] RThighState(){
+		float[] state = new float[3];
+		state[0] = game.RThighBody.getPosition().x;
+		state[1] = game.RThighBody.getPosition().y;
+		state[2] = game.RThighBody.getAngle();
+		
+		return state;
+	}
+	/** State getters below - The convention is to use x-y-theta **/
+	public float[] LCalfState(){
+		float[] state = new float[3];
+		state[0] = game.LCalfBody.getPosition().x;
+		state[1] = game.LCalfBody.getPosition().y;
+		state[2] = game.LCalfBody.getAngle();
+		
+		return state;
+	}
+	/** State getters below - The convention is to use x-y-theta **/
+	public float[] RCalfState(){
+		float[] state = new float[3];
+		state[0] = game.RCalfBody.getPosition().x;
+		state[1] = game.RCalfBody.getPosition().y;
+		state[2] = game.RCalfBody.getAngle();
+		
+		return state;
+	}
+	/** State getters below - The convention is to use x-y-theta **/
+	public float[] LFootState(){
+		float[] state = new float[3];
+		state[0] = game.LFootBody.getPosition().x;
+		state[1] = game.LFootBody.getPosition().y;
+		state[2] = game.LFootBody.getAngle();
+		
+		return state;
+	}
+	/** State getters below - The convention is to use x-y-theta **/
+	public float[] RFootState(){
+		float[] state = new float[3];
+		state[0] = game.RFootBody.getPosition().x;
+		state[1] = game.RFootBody.getPosition().y;
+		state[2] = game.RFootBody.getAngle();
+		
+		return state;
+	}
+	/** State getters below - The convention is to use x-y-theta **/
+	public float[] LUArmState(){
+		float[] state = new float[3];
+		state[0] = game.LUArmBody.getPosition().x;
+		state[1] = game.LUArmBody.getPosition().y;
+		state[2] = game.LUArmBody.getAngle();
+		
+		return state;
+	}
+	/** State getters below - The convention is to use x-y-theta **/
+	public float[] RUArmState(){
+		float[] state = new float[3];
+		state[0] = game.RUArmBody.getPosition().x;
+		state[1] = game.RUArmBody.getPosition().y;
+		state[2] = game.RUArmBody.getAngle();
+		
+		return state;
+	}
+	/** State getters below - The convention is to use x-y-theta **/
+	public float[] LLArmState(){
+		float[] state = new float[3];
+		state[0] = game.LLArmBody.getPosition().x;
+		state[1] = game.LLArmBody.getPosition().y;
+		state[2] = game.LLArmBody.getAngle();
+		
+		return state;
+	}
+	/** State getters below - The convention is to use x-y-theta **/
+	public float[] RLArmState(){
+		float[] state = new float[3];
+		state[0] = game.RLArmBody.getPosition().x;
+		state[1] = game.RLArmBody.getPosition().y;
+		state[2] = game.RLArmBody.getAngle();
+		
+		return state;
+	}
+	
+	/** Get Dist/phys steps- for checking if we're on some super-slow walk **/
 	public float NormSpeed(){
 		float normspeed = game.TorsoBody.getPosition().x/stepsInRun;
 		return normspeed;
 	}
 
-	/* Cost function */
+	/** Cost function **/
 	public float Cost(){
 		float cost = OptionsHolder.CostFunction(game); //Cost function now stored in OptionsHOlder for easier access.
 //		return -game.TorsoBody.getPosition().x;
 		return cost;
 	}
 	
-	/* Check failure based on state */
+	/** Check failure based on state **/
 	public boolean CheckFailure(){
 //		boolean failure = game.TorsoBody.getPosition().y>5; //ground is at 10ish with up being -, so we're calling torso above 7 being failure.	
 		boolean failure = OptionsHolder.FailureCondition(game); //Failure condition now stored in OptionsHolder for easier access.
 		return failure;
 	}
 	
-	/* Do sequence of actions */
+	/** Do sequence of actions **/
 	public float[] DoSequence(int delay[]) throws InterruptedException{
 		float [] cost = new float[delay.length];
 		
